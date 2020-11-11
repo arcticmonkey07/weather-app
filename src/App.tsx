35e107dbcf198516c99bea876ebc96d1f20f4030
arcticmonkey07 <arcticmonkey07@gmail.com>
2020-11-11 16:28:17 +0300

    add styles, city array

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,14 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 import './App.css';
-import Geocode from "react-geocode";
 
 import Main from './components/Main';
 
-const GOOGLE_API_KEY = 'AIzaSyDhO6aEwEWbUCVvSFRffBE5t2ZLtxks_vU';
-const OPEN_WEATHER_KEY = 'd0d3f8027069eff90e6ce91c8e818d52';
-
 const App: FC = () => {
-  const [city, setCity] = useState<string>('');
-  const [timezone, setTimezone] = useState<string>('');
 
-  useEffect(() => {
+  // useEffect(() => {
     // navigator.geolocation.getCurrentPosition(position => setLatitude(position.coords.latitude));
     // navigator.geolocation.getCurrentPosition(position => setLongitude(position.coords.longitude));
-  }, [])
-
-  Geocode.setApiKey(GOOGLE_API_KEY);
-  Geocode.setLanguage("ru");
-  Geocode.setRegion("ru");
-
-  const getLatAndLong = (city) => {
-    Geocode.fromAddress(city).then(
-      response => {
-        const { lat, lng } = response.results[0].geometry.location;
-        let api = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly&appid=${OPEN_WEATHER_KEY}`
-
-        fetch(api)
-          .then(response => response.json())
-          .then(data => console.log(data))
-      },
-      error => {
-        console.error(error);
-      }
-    );
-  };
-
-  const formHandler = (e) => {
-    e.preventDefault();
-    getLatAndLong(city);
-  }
+  // }, [])
 
   // const getGeolocation = () => {
   //   return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}`)
@@ -48,17 +17,8 @@ const App: FC = () => {
   // }
 
   return (
-    <div>
-      <p>Ваш город?</p>
-      {/* <button onClick={getGeolocation}>yes</button>
-      <button>no</button>
-      <p>{city}</p> */}
-
-      <form onSubmit={formHandler}>
-        <input onChange={(e) => setCity(e.target.value)} />
-        <button type='submit'>send</button>
-      </form>
-      {/* {timezone} */}
+    <div className='app'>
+      <Main />
     </div>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,54 @@
 import React, { FC, useState } from 'react';
-import { Input  } from 'antd';
+import './Main.css';
+import { Input } from 'antd';
+import Geocode from "react-geocode";
 
 const { Search } = Input;
 
-const Main: FC = () => {
-  const [cities, setCities] = useState<any>([]);
+const GOOGLE_API_KEY = 'AIzaSyDhO6aEwEWbUCVvSFRffBE5t2ZLtxks_vU';
+const OPEN_WEATHER_KEY = 'd0d3f8027069eff90e6ce91c8e818d52';
 
-  const onSearch = value => setCities(prev => [...prev, value]);
+const Main: FC = () => {
+  const [city, setCity] = useState<string>('');
+  const [timezone, setTimezone] = useState<any>([]);
+
+  Geocode.setApiKey(GOOGLE_API_KEY);
+  Geocode.setLanguage("ru");
+  Geocode.setRegion("ru");
+
+  const getLatAndLong = (city) => {
+    Geocode.fromAddress(city).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        const { long_name } = response.results[0].address_components[0];
+        setTimezone(prev => [...prev, long_name]);
+        
+        let api = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly&appid=${OPEN_WEATHER_KEY}`
+
+        fetch(api)
+          .then(response => response.json())
+          .then(data => console.log(data))
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  };
+
+  const onAdd = () => {
+    getLatAndLong(city);
+  }
 
   return (
-    <div>
-      <Search placeholder="input search text" onSearch={onSearch} enterButton />
-      <ul>
-        {cities.map(item => <li key={item}>{item}</li>)}
-      </ul>
+    <div className='container'>
+      <div className="main">
+        <h2>Добавьте город</h2>
+        <Search className='input' placeholder="input city" onSearch={onAdd} onChange={(e) => setCity(e.target.value)} enterButton />
+        {/* <p className='city'>{timezone}</p> */}
+        <ul>
+          {timezone.map(item => <li className='city' key={item.lat}>{item}</li>)}
+        </ul>
+      </div>
     </div>
   )
 }
